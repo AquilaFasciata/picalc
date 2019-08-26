7da73ba8f9de588ff02281ad880fffb48fb01075
Zach Saunders <zachisaunders12@gmail.com>
2019-08-26 18:33:14 -0400

    Windows initialization

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,9 @@
-public class Calculator {
-    public static void main(String[] args) {
-        int num1;
-        int num2;
-        String mathFunction;
-        for (String s: args) {
-            num1 = Integer.parseInt(s[0]);
-            num2 = Integer.parseInt(s[1]);
-            mathFunction = s[3];
-        }
+import java.math.*;
+import java.util.Scanner;
 
+public class Calculator {
+    public static void Main(String[] args) {
+        double pi = Math.PI;
+        
+        }
     }
-}
